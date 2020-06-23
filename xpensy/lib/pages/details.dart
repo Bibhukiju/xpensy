0a1aca389291f8dbe82a59270ac08b4e7806e119
bibhukiju <bibhukiju@gmail.com>
2020-06-23 16:12:40 +0545

    details pages is re created

--- xpensy/lib/pages/details.dart
+++ xpensy/lib/pages/details.dart
@@ -1,234 +1,148 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:xpensy/Helpers/dbhelper.dart';
 import 'package:xpensy/models/expenseModel.dart';
 import 'package:xpensy/pages/editpage.dart';
 import 'package:xpensy/pages/homescreen.dart';
 
-class Details extends StatelessWidget {
+class Details extends StatefulWidget {
   final Expenses expenses;
   Details({this.expenses});
+
+  @override
+  _DetailsState createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
-    return expenses == null
-        ? CircularProgressIndicator()
-        : Scaffold(
-            body: ListView(
-              children: <Widget>[
-                AppBar(
-                  title: Text(expenses.desc),
-                  centerTitle: true,
+    delete() {
+      showDialog(
+          context: context,
+          child: SimpleDialog(
+            title: Text("Do you want to delete"),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text(
+                  "Delete",
+                  style: TextStyle(color: Colors.red),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height / 3,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.circular(20)),
-                    child: expenses.photoname == " "
-                        ? Hero(
-                            tag: expenses.id,
-                            child: Center(
-                                child: Container(
-                                    child: Icon(
-                              Icons.broken_image,
-                              size: MediaQuery.of(context).size.height / 10,
-                              color: Colors.black.withOpacity(.5),
-                            ))))
-                        : Hero(
-                            tag: expenses.id,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.memory(
-                                base64Decode(expenses.photoname),
-                                fit: BoxFit.fitWidth,
-                              ),
-                            ),
+                onPressed: () async {
+                  await DBHelper.instance.delete(widget.expenses.id);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => HomeScreen()));
+                },
+              ),
+              SimpleDialogOption(
+                child: Text("cancel"),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          ));
+    }
+
+    navigate() {
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) => Editpage(
+                expenses: widget.expenses,
+              )));
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.expenses.desc),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Hero(
+                tag: widget.expenses.id,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all()),
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: widget.expenses.photoname == " "
+                      ? Icon(
+                          Icons.broken_image,
+                          color: Theme.of(context).primaryColor,
+                          size: MediaQuery.of(context).size.height / 4,
+                        )
+                      : ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.memory(
+                            base64Decode(widget.expenses.photoname),
+                            fit: BoxFit.fitWidth,
                           ),
-                  ),
+                        ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: Container(
-                              height: MediaQuery.of(context).size.height / 5,
-                              child: Card(
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Center(
-                                      child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Text(
-                                        "Amount",
-                                        style: TextStyle(
-                                            color: Colors.deepPurple,
-                                            fontSize: 25,
-                                            fontWeight: FontWeight.w300),
-                                      ),
-                                      Divider(
-                                        color: Colors.grey[500],
-                                      ),
-                                      Text(
-                                        expenses.amount,
-                                        style: int.parse(expenses.amount) >=
-                                                1000
-                                            ? TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.red)
-                                            : TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.green,
-                                                fontWeight: FontWeight.w300),
-                                      ),
-                                    ],
-                                  ))))),
-                      Expanded(
-                          child: Container(
-                              height: MediaQuery.of(context).size.height / 5,
-                              child: Card(
-                                  elevation: 3,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child: Center(
-                                      child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Text(
-                                        "Date",
-                                        style: TextStyle(
-                                            color: Colors.deepPurple,
-                                            fontSize: 25,
-                                            fontWeight: FontWeight.w300),
-                                      ),
-                                      Divider(
-                                        color: Colors.grey[500],
-                                      ),
-                                      Text(
-                                        expenses.date,
-                                        style: TextStyle(
-                                            color: Colors.green,
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w300),
-                                      ),
-                                    ],
-                                  ))))),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          child: Container(
-                              height: MediaQuery.of(context).size.height / 5,
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (BuildContext context) =>
-                                          Editpage(
-                                            expenses: expenses,
-                                          )));
-                                },
-                                child: Card(
-                                    elevation: 3,
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Center(
-                                        child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceEvenly,
-                                      children: <Widget>[
-                                        Icon(
-                                          Icons.mode_edit,
-                                          color: Colors.green,
-                                          size: 45,
-                                        ),
-                                        Divider(
-                                          color: Colors.grey[500],
-                                        ),
-                                        Text("Ëdit",
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w300,
-                                                color: Colors.red)),
-                                      ],
-                                    ))),
-                              ))),
-                      Expanded(
-                          child: GestureDetector(
-                        onTap: () {
-                          showDialog(
-                              context: context,
-                              child: SimpleDialog(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                title: Text("Do you want to delete?"),
-                                contentPadding: EdgeInsets.all(10),
-                                titlePadding: EdgeInsets.all(10),
-                                children: <Widget>[
-                                  SimpleDialogOption(
-                                    child: Text(
-                                      "Delete",
-                                      style: TextStyle(color: Colors.red),
-                                    ),
-                                    onPressed: () {
-                                      DBHelper.instance.delete(expenses.id);
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (BuildContext context) =>
-                                                  HomeScreen()));
-                                    },
-                                  ),
-                                  SimpleDialogOption(
-                                    child: Text("cancel"),
-                                    onPressed: () =>
-                                        Navigator.of(context).pop(),
-                                  )
-                                ],
-                              ));
-                        },
-                        child: Container(
-                            height: MediaQuery.of(context).size.height / 5,
-                            child: Card(
-                                elevation: 3,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Center(
-                                    child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    Icon(
-                                      Icons.delete_outline,
-                                      color: Colors.red,
-                                      size: 45,
-                                    ),
-                                    Divider(
-                                      color: Colors.grey[500],
-                                    ),
-                                    Text("Delete",
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w300,
-                                            color: Colors.red)),
-                                  ],
-                                )))),
-                      )),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          );
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      detailsRow(data: widget.expenses.amount, title: "Amount"),
+                      detailsRow(
+                          title: "Category", data: widget.expenses.catergory),
+                      detailsRow(
+                          title: "Date of expenses",
+                          data: widget.expenses.date),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          createButton(context, delete,
+                              col: Colors.red, ico: Icons.delete_outline),
+                          createButton(context, navigate,
+                              ico: Icons.mode_edit, col: Colors.green)
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+
+  OutlineButton createButton(BuildContext context, Function fun,
+      {IconData ico, Color col}) {
+    return OutlineButton(
+      borderSide: BorderSide(
+        color: Colors.teal,
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      onPressed: fun,
+      child: Icon(
+        ico,
+        color: col,
+      ),
+    );
+  }
+
+  Row detailsRow({@required String title, @required String data}) {
+    return Row(
+      children: <Widget>[
+        Text(
+          title + ":  ",
+          style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+        ),
+        Text(data),
+      ],
+    );
   }
 }
